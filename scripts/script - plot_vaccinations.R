2f1e3d5392ff438e1ec491c45cbb05d83b4a8888
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-01-29 17:45:52 -0600

    Update script - plot_vaccinations.R

--- scripts/script - plot_vaccinations.R
+++ scripts/script - plot_vaccinations.R
@@ -1,18 +1,18 @@
 devtools::load_all()
 
-p <- plot_vaccinations(
-  n_partial = 26688,
-  n_full = 6999,
-  date_updated = as.Date("2020-01-18")
+p <- plot_vaccinations2(
+  date_updated = "2021-01-28"
 ) %T>% show()
 
+fname <- paste0(
+  "V:/EPI DATA ANALYTICS TEAM/COVID SANDBOX REDCAP DATA/",
+  "COVID-19 Vaccine Reporting/figs/vaccination_goal/vaccination_goal_",
+  vac_date_updated(),
+  ".png"
+)
+
 ggplot2::ggsave(
-  paste0(
-    "V:/EPI DATA ANALYTICS TEAM/COVID SANDBOX REDCAP DATA/",
-    "COVID-19 Vaccine Reporting/figs/vaccine_slide/vaccination_goal_",
-    Sys.Date(),
-    ".png"
-  ),
+  fname,
   p,
   width = 12,
   height = 9
