7fc8fc3346fbe250c19a2cd65ac558ecde9294ed
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-01-29 17:45:33 -0600

    Create vac_count.R

---
+++ R/vac_count.R
@@ -0,0 +1,23 @@
+#' Count Vaccinations by Dose
+#'
+#' @param .data Vaccination data; default loads and parses the latest file
+#'
+#' @param resident_only Should counts include Shelby County residents only? If
+#'   so, persons of unknown residency will still be included.
+#'
+#' @return A `tibble` with columns `dose_count` and `n`
+vac_count <- function(
+  .data = coviData::vac_load() %>% coviData::vac_prep(),
+  resident_only = TRUE
+) {
+
+  .data %>%
+    purrr::when(
+      resident_only ~ dplyr::filter(
+        .,
+        .data[["resident"]] | is.na(.data[["resident"]])
+      ),
+      ~ .
+    ) %>%
+    dplyr::count(.data[["dose_count"]])
+}
