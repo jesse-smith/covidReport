c1d4c87d27c6574850daec3d07930c384fcf123a
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-31 15:40:55 -0500

    Document `pop_age_2019`

---
+++ R/pop_age_2019.R
@@ -0,0 +1,12 @@
+#' Shelby County Age Demographics - ACS 2019 1-Year Estimates
+#'
+#' @format A `tibble` with 8 rows and 2 variables:
+#'   \describe{
+#'     \item{age_grp}{age group, in years (closed on left, open on right)}
+#'     \item{n}{Number of people in age group}
+#'   }
+#'
+#' @source \href{
+#'     https://data.census.gov/cedsci/table?q=ACSDP5Y2019.DP05%20Shelby%20County,%20Tennessee&g=0500000US47157&tid=ACSDP1Y2019.DP05&hidePreview=true
+#'   }{US Census Bureau}
+"pop_age_2019"
