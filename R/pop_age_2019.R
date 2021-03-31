bbc30b026db2e23270806e4936bc95448f131daf
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-31 15:44:42 -0500

    Update `pop_age_2019` source

--- R/pop_age_2019.R
+++ R/pop_age_2019.R
@@ -6,7 +6,5 @@
 #'     \item{n}{Number of people in age group}
 #'   }
 #'
-#' @source \href{
-#'     https://data.census.gov/cedsci/table?q=ACSDP5Y2019.DP05%20Shelby%20County,%20Tennessee&g=0500000US47157&tid=ACSDP1Y2019.DP05&hidePreview=true
-#'   }{US Census Bureau}
+#' @source US ACS Census Estimates 2019
 "pop_age_2019"
