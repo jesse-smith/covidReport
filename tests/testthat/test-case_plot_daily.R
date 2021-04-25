5a6c5f69da1c07f2fd9af2f106cf81530b64da6e
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-04-25 17:31:08 -0500

    Test `case_plot_daily()`

---
+++ tests/testthat/test-case_plot_daily.R
@@ -0,0 +1,29 @@
+test_that("`case_plot_daily()` returns expected graphic", {
+  mockery::stub(
+    case_plot_daily,
+    "coviData::process_positive_people",
+    tibble::tibble(.rows = 93895L)
+  )
+
+  report_date <- readRDS(testthat::test_path("../data/test_report_data.rds"))
+
+  mockery::stub(
+    case_plot_daily,
+    "coviData::load_report_date",
+    report_date
+  )
+
+  data <- dplyr::left_join(
+    readRDS(testthat::test_path("../data/test_cumulative_data.rds")),
+    report_date,
+    by  = "inv_local_id"
+  ) %>%
+    dplyr::transmute(
+      .data[["inv_local_id"]],
+      specimen_coll_dt = lubridate::as_date(.data[["collection_date"]])
+    )
+
+  plt <- case_plot_daily(data, date = "2021-04-19")
+
+  vdiffr::expect_doppelganger(title = "daily cases", fig = plt)
+})
