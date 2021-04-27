2ffddd93e4ebb1b42c20424b6275bb02d951c039
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-04-27 09:49:15 -0500

    Create `case_table_active()`

---
+++ R/case_table_active.R
@@ -0,0 +1,86 @@
+case_table_active <- function(
+  data = coviData::process_positive_people(date = date),
+  date = NULL
+) {
+  date <- coviData::path_inv(date = date) %>%
+    fs::path_file() %>%
+    fs::path_ext_remove() %>%
+    stringr::str_extract("[0-9]{1,4}.?[0-9]{1,2}.?[0-9]{1,4}") %>%
+    coviData::std_dates(orders = "ymd", force = "dt")
+
+  a_cols <- c("illness_onset_dt", "specimen_coll_dt", "inv_start_dt")
+
+  data %>%
+    dplyr::mutate(.id_tmp_ = dplyr::row_number()) %>%
+    dplyr::mutate(
+      active_dt = dplyr::across(
+        {{ a_cols }},
+        ~ coviData::std_dates(.x, orders = "ymdT", train = FALSE, force = "dt")
+      ) %>%
+        dplyr::transmute(dt = coviData::coalesce_across({{ a_cols }})) %>%
+        dplyr::pull("dt"),
+      active_days = as.integer({{ date }} - .data[["active_dt"]]),
+      active = dplyr::between(.data[["active_days"]], 0L, 14L),
+      died = .data[[".id_tmp_"]] %in% filter_deaths(.)[[".id_tmp_"]]
+    ) %>%
+    dplyr::transmute(
+      status = dplyr::case_when(
+        .data[["died"]] ~ "Deceased",
+        .data[["active"]] ~ "Active",
+        TRUE ~ "Inactive/Recovered"
+      )
+    ) %>%
+    janitor::tabyl(.data[["status"]]) %>%
+    dplyr::mutate(
+      percent = .data[["percent"]] %>%
+        round(digits = 3) %>%
+        vec_assign(i = 3L, 1 - vec_slice(., 1L) - vec_slice(., 2L))
+    ) %>%
+    janitor::adorn_totals() %>%
+    gt::gt() %>%
+    fmt_covid_table() %>%
+    gt::cols_label(status = "Status", n = "N", percent = "%") %>%
+    gt::fmt_number("n", decimals = 0L) %>%
+    gt::fmt_percent("percent", decimals = 1L) %>%
+    gt::cols_align("right") %>%
+    gt::tab_style(
+      style = gt::cell_text(align = "center"),
+      locations = list(
+        gt::cells_column_labels(gt::everything()),
+        gt::cells_body("status")
+      )
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(weight = "bold"),
+      locations = list(
+        gt::cells_column_labels(gt::everything()),
+        gt::cells_body(columns = "status"),
+        gt::cells_body(rows = 4L)
+      )
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_borders(sides = c("top", "bottom"), weight = NULL),
+      locations = gt::cells_body(rows = 1:3)
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_borders(sides = c("left", "right"), weight = NULL),
+      locations = list(
+        gt::cells_column_labels("n"),
+        gt::cells_body(columns = "n")
+      )
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_borders(sides = "right", weight = NULL),
+      locations = list(
+        gt::cells_column_labels("status"),
+        gt::cells_body(columns = "status")
+      )
+    ) %>%
+     gt::tab_style(
+       style = gt::cell_borders(sides = "left", weight = NULL),
+       locations = list(
+         gt::cells_column_labels("percent"),
+         gt::cells_body(columns = "percent")
+       )
+     )
+}
