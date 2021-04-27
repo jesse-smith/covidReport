9bd114d6e0c6b3ef6279d80427302b96860a161b
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-04-26 19:13:08 -0500

    Document and export `death_table_age()`

--- R/death_table_age.R
+++ R/death_table_age.R
@@ -1,3 +1,13 @@
+#' Tabulate Age Median and Range for COVID-19 Deaths
+#'
+#' @param data Case data, as output by
+#'   \code{\link[coviData:process_positive_people]{process_positive_people()}}
+#'
+#' @param date The download date of the data; defaults to most recent
+#'
+#' @return A `gt_tbl`
+#'
+#' @export
 death_table_age <- function(
   data = coviData::process_positive_people(),
   date = NULL
