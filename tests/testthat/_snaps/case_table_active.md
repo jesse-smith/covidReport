01d66c0d3b51b2c3448b0572a1a8eeb14674160b
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-05-03 18:05:15 -0500

    Test `case_table_active()` info and html separately

---
+++ tests/testthat/_snaps/case_table_active.md
@@ -0,0 +1,9 @@
+# `case_table_active()` html matches snapshot
+
+    Code
+      tbl_html
+    Output
+      {html_document}
+      <html>
+      [1] <body><table style="font-family: -apple-system, BlinkMacSystemFont, 'Sego ...
+
--- tests/testthat/test-case_table_active.R
+++ tests/testthat/test-case_table_active.R
@@ -1,4 +1,4 @@
-test_that("`case_table_active()` works with active cases", {
+test_that("`case_table_active()` info matches ref (with active cases)", {
 
   path_fn <- function(
     date = NULL,
@@ -63,7 +63,7 @@ test_that("`case_table_active()` works with active cases", {
   expect_equal(tbl_active, tbl_ref)
 })
 
-test_that("`case_table_active()` works with no active cases", {
+test_that("`case_table_active()` info matches ref (with no active cases)", {
 
   path_fn <- function(
     date = NULL,
@@ -137,4 +137,58 @@ test_that("`case_table_active()` works with no active cases", {
   )
 
   expect_equal(tbl_active, tbl_ref)
+})
+
+test_that("`case_table_active()` html matches snapshot", {
+
+  path_fn <- function(
+    date = NULL,
+    dir = fs::path(
+      "V:/EPI DATA ANALYTICS TEAM/COVID SANDBOX REDCAP DATA",
+      "Sandbox data pull Final"
+    ),
+    ext = c("csv", "xlsx", "fst")
+  ) {
+    ext <- rlang::arg_match(ext)[[1L]]
+    if (is.null(date)) date <- as.character(lubridate::today()-1L)
+    date <- as.character(lubridate::as_date(date))
+
+    fs::path(dir, paste0(date, " Final Data Pull.", ext))
+  }
+
+  mockery::stub(
+    case_table_active,
+    "coviData::path_inv",
+    path_fn
+  )
+
+  data <- tibble::tibble(
+    die_from_illness_ind = rep(
+      c("Y", rep("N", 3L), rep("U", 3L), rep(NA, 3L)),
+      1e4L
+    ),
+    inv_death_dt = seq(
+      as.Date("2020-03-26"),
+      as.Date("2021-03-26"),
+      length.out = 99999L
+    ) %>% purrr::prepend(lubridate::NA_Date_),
+    illness_onset_dt = c(rep(as.Date("2020-03-10"), 99e3L), rep("2021-03-26", 1e3L)),
+    specimen_coll_dt = illness_onset_dt,
+    inv_start_dt = specimen_coll_dt
+  ) %>%
+    dplyr::mutate(
+      dplyr::across(
+        where(lubridate::is.Date),
+        ~ format(.x, "%Y-%m-%dT%H:%M:%SZ")
+      )
+    ) %>%
+    dplyr::slice_sample(prop = 1)
+
+  tbl_html <- suppressWarnings(
+    case_table_active(data, date = "2021-04-01") %>%
+      gt::as_raw_html() %>%
+      xml2::read_html()
+  )
+
+  expect_snapshot(tbl_html)
 })
