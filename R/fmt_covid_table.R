a41c0adff64470b61beca7c3b0ce2fe81bbcc0ea
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-05-13 14:18:38 -0500

    Define defaults in `fmt_covid_table()` for {flextable}

--- R/fmt_covid_table.R
+++ R/fmt_covid_table.R
@@ -1,20 +1,28 @@
-fmt_covid_table <- function(gt_tbl) {
-  gt_tbl %>%
-    gt::tab_style(
-      style = list(
-        gt::cell_text(color = "white", v_align = "top"),
-        gt::cell_fill(color = "midnightblue"),
-        gt::cell_borders()
-      ),
-      locations = gt::cells_column_labels(gt::everything())
-    ) %>%
-    gt::tab_style(
-      style = gt::cell_borders(),
-      locations = list(
-        gt::cells_column_labels(gt::everything()),
-        gt::cells_body()
-      )
-    ) %>%
-    gt::opt_table_lines("none") %>%
-    gt::tab_options()
+fmt_covid_table <- function(flextable, total = FALSE) {
+
+  inner_border <- officer::fp_border("grey60")
+  outer_border <- officer::fp_border("grey30", width = 2)
+
+  covid_flextable <- flextable %>%
+    # Background
+    flextable::bg(bg = "#f0f0f0", part = "all") %>%
+    flextable::bg(bg = "midnightblue", part = "header") %>%
+    # Font color
+    flextable::color(color = "#f0f0f0", part = "header") %>%
+    flextable::color(color = "grey30", part = "body") %>%
+    flextable::color(color = "grey60", part = "footer") %>%
+    # Font boldness
+    flextable::bold(part = "header") %>%
+    flextable::bold(j = 1L, part = "body") %>%
+    # Borders
+    flextable::border_remove() %>%
+    flextable::border_inner_h(inner_border) %>%
+    flextable::hline_bottom(border = outer_border)
+
+  if (!total) return(covid_flextable)
+
+  covid_flextable %>%
+    flextable::bold(i = flextable::nrow_part(.)) %>%
+    flextable::hline_top(i = flextable::nrow_part(.), border = outer_border) %>%
+    flextable::hline_bottom(i = flextable::nrow_part(.), border = outer_border)
 }
