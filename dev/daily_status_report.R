0c0096713b507f6b0e68c67d0d8e0715f94c5c89
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-05-14 12:29:51 -0500

    Update template and daily report script

--- dev/daily_status_report.R
+++ dev/daily_status_report.R
@@ -1,7 +1,24 @@
 # Data
-pos_ppl <- coviData::process_positive_people()
+pos_ppl <- dplyr::select(
+  coviData::process_positive_people(),
+  "inv_local_id",
+  "inv_case_status",
+  "specimen_coll_dt",
+  "patient_dob",
+  "die_from_illness_ind",
+  "illness_onset_dt",
+  "inv_start_dt",
+  "inv_death_dt",
+  "age_in_years",
+  "investigation_status_cd"
+)
 gc()
-pcr <- coviData::read_file_delim(coviData::path_pcr())
+pcr <- dplyr::select(
+  janitor::clean_names(coviData::read_file_delim(coviData::path_pcr())),
+  "inv_local_id",
+  "specimen_coll_dt",
+  "lab_result"
+)
 gc()
 
 # Cumulative case slide
@@ -38,13 +55,21 @@ gc()
 inv_tbl_total <- inv_table_total(pos_ppl)
 gc()
 
+remove(pos_ppl, pcr)
+gc()
+
 # Initialize Powerpoint
 master <- "HD Blue and White"
+date   <- coviData::path_inv() %>%
+  fs::path_file() %>%
+  fs::path_ext_remove() %>%
+  stringr::str_extract("[0-9]{1,4}.?[0-9]{1,2}.?[0-9]{1,4}") %>%
+  lubridate::as_date() %>%
+  format("%B %d, %Y")
 pptx <- officer::read_pptx("dev/covid_report_template.pptx")
 
 # Create title slide
 title <- "COVID-19 Daily Status Report"
-date  <- format(Sys.Date(), "%B %d, %Y")
 pptx <- pptx %>%
   officer::add_slide("Title Slide", master) %>%
   officer::ph_with(
@@ -75,31 +100,134 @@ pptx <- pptx %>%
 # Create confirmed/probable slide
 cp_title <- "COVID-19 Cases and Deaths by Status"
 pptx <- pptx %>%
-  officer::add_slide("Title and Content", master) %>%
+  officer::add_slide("Table", master) %>%
   officer::ph_with(
     value = cp_title,
     location = officer::ph_location_type("title")
   ) %>%
   officer::ph_with(
+    value = date,
+    location = officer::ph_location_type("subTitle")
+  ) %>%
+  officer::ph_with(
     value = case_tbl_confirmed_probable,
-    location = officer::ph_location_type("body")
+    location = ph_location_table(
+      case_tbl_confirmed_probable,
+      pptx,
+      layout = "Table",
+      valign = 1
+    )
   )
 
 # Create deaths slide
 deaths_title <- "COVID-19 Deaths"
 pptx <- pptx %>%
-  officer::add_slide("Title and Content", master) %>%
+  officer::add_slide("Two Table Vertical", master) %>%
   officer::ph_with(
     value = deaths_title,
     location = officer::ph_location_type("title")
   ) %>%
   officer::ph_with(
+    value = date,
+    location = officer::ph_location_type("subTitle")
+  ) %>%
+  officer::ph_with(
     value = death_tbl_total,
-    location = officer::ph_location_type("body")
+    location = ph_location_table(
+      death_tbl_total,
+      pptx,
+      layout = "Two Table Vertical",
+      pos_h = FALSE,
+      valign = 1
+    )
   ) %>%
   officer::ph_with(
     value = death_tbl_age,
-    location = officer::ph_location_type("body")
+    location = ph_location_table(
+      death_tbl_age,
+      pptx,
+      layout = "Two Table Vertical",
+      pos_h = FALSE,
+      pos_first = FALSE,
+      valign = 1
+    )
+  )
+
+# Create active slide
+active_title <- "Active COVID-19 Cases"
+pptx <- pptx %>%
+  officer::add_slide("Table", master) %>%
+  officer::ph_with(
+    value = active_title,
+    location = officer::ph_location_type("title")
+  ) %>%
+  officer::ph_with(
+    value = date,
+    location = officer::ph_location_type("subTitle")
+  ) %>%
+  officer::ph_with(
+    value = case_tbl_active,
+    location = ph_location_table(
+      case_tbl_active,
+      pptx,
+      layout = "Table",
+      pos_h = FALSE,
+      valign = 1
+    )
+  )
+
+# Create test slide
+test_title <- "COVID-19 PCR Tests"
+pptx <- pptx %>%
+  officer::add_slide("Table", master) %>%
+  officer::ph_with(
+    value = test_title,
+    location = officer::ph_location_type("title")
+  ) %>%
+  officer::ph_with(
+    value = date,
+    location = officer::ph_location_type("subTitle")
+  ) %>%
+  officer::ph_with(
+    value = test_tbl_total,
+    location = ph_location_table(
+      test_tbl_total,
+      pptx,
+      layout = "Table",
+      pos_h = FALSE,
+      valign = 1
+    )
+  )
+
+# Create positivity slide
+pptx <- pptx %>%
+  officer::add_slide("Picture only", master) %>%
+  officer::ph_with(
+    value = test_plt_pos,
+    location = officer::ph_location_type("pic")
+  )
+
+# Create investigations slide
+inv_title <- "COVID-19 Case Investigations"
+pptx <- pptx %>%
+  officer::add_slide("Table", master) %>%
+  officer::ph_with(
+    value = inv_title,
+    location = officer::ph_location_type("title")
+  ) %>%
+  officer::ph_with(
+    value = date,
+    location = officer::ph_location_type("subTitle")
+  ) %>%
+  officer::ph_with(
+    value = inv_tbl_total,
+    location = ph_location_table(
+      inv_tbl_total,
+      pptx,
+      layout = "Table",
+      pos_h = FALSE,
+      valign = 1
+    )
   )
 
 print(pptx, target = "dev/officer_report.pptx")
