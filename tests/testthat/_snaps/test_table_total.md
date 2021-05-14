938a5c104f4b1fbc98a38ec502c500b6cd04eb3a
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-05-14 09:26:11 -0500

    Use flextable with `test_table_total()`

--- tests/testthat/_snaps/test_table_total.md
+++ tests/testthat/_snaps/test_table_total.md
@@ -1,9 +1,15 @@
-# `test_table_total()` html matches snapshot
+# `void(test_table_total())` matches snapshot
 
     Code
-      tbl_html
+      tbl_void
     Output
-      {html_document}
-      <html>
-      [1] <body><table style="font-family: -apple-system, BlinkMacSystemFont, 'Sego ...
+      a flextable object.
+      col_keys: `result`, `N`, `percent` 
+      header has 1 row(s) 
+      body has 3 row(s) 
+      original dataset sample: 
+          result       N percent
+      1 Positive  100000      10
+      2 Negative  900000      90
+      3    Total 1000000     100
 
--- tests/testthat/test-test_table_total.R
+++ tests/testthat/test-test_table_total.R
@@ -16,26 +16,26 @@ test_that("`test_table_total()` info matches reference", {
     tibble::tibble(.rows = 9e5L)
   )
 
-  tbl_test <- suppressWarnings(
-    test_table_total(data) %>%
-      gt::as_raw_html() %>%
+  tbl_test <- test_table_total(data) %>%
+      flextable::flextable_to_rmd() %>%
       xml2::read_html() %>%
       rvest::html_node("table") %>%
       rvest::html_table() %>%
-      dplyr::as_tibble()
-  )
+      dplyr::as_tibble() %>%
+      set_colnames(janitor::make_clean_names(.[1L,])) %>%
+      dplyr::filter(dplyr::row_number() > 1L)
 
   tbl_ref <- tibble::tribble(
-    ~ `PCR Result`,         ~ N,         ~ `%`,
-        "Positive",   "100,000",  "10&percnt;",
-        "Negative",   "900,000",  "90&percnt;",
-           "Total", "1,000,000", "100&percnt;"
+    ~ `pcr_result`,         ~ n, ~ `percent`,
+        "Positive",   "100,000",       "10.0%",
+        "Negative",   "900,000",       "90.0%",
+           "Total", "1,000,000",      "100.0%"
   )
 
   expect_equal(tbl_test, tbl_ref)
 })
 
-test_that("`test_table_total()` html matches snapshot", {
+test_that("`void(test_table_total())` matches snapshot", {
   # Make fake data to pass to function
   data <- tibble::tibble(.rows = 1e6L)
 
@@ -53,11 +53,10 @@ test_that("`test_table_total()` html matches snapshot", {
     tibble::tibble(.rows = 9e5L)
   )
 
-  tbl_html <- suppressWarnings(
-    test_table_total(data) %>%
-      gt::as_raw_html() %>%
-      xml2::read_html()
+  tbl_void <- flextable::void(
+    test_table_total(data),
+    part = "all"
   )
 
-  expect_snapshot(tbl_html)
+  expect_snapshot(tbl_void)
 })
