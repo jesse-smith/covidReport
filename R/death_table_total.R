9a98735ba9380f6b1d623d119530834af613fb22
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-05-18 17:16:12 -0500

    Separate `death_calc_total()`

--- R/death_table_total.R
+++ R/death_table_total.R
@@ -13,15 +13,8 @@ death_table_total <- function(
   date = NULL
 ) {
   data %>%
-    dplyr::mutate(.id_tmp_ = dplyr::row_number()) %>%
-    dplyr::transmute(
-      .died_tmp_ = .data[[".id_tmp_"]] %in% filter_deaths(.)[[".id_tmp_"]],
-      died = dplyr::if_else(.data[[".died_tmp_"]], "Yes", "No")
-    ) %>%
-    janitor::tabyl(.data[["died"]]) %>%
-    janitor::adorn_totals() %>%
+    death_calc_total(date = date) %>%
     dplyr::mutate(percent = 100 * .data[["percent"]]) %>%
-    dplyr::arrange(c(2L, 1L, 3L)) %>%
     flextable::flextable() %>%
     flextable::set_header_labels(
       died = "COVID-19 Deaths",
@@ -31,4 +24,27 @@ death_table_total <- function(
     fmt_covid_table(total = TRUE) %>%
     flextable::colformat_double(j = "percent", digits = 1L, suffix = "%") %>%
     flextable::autofit()
+}
+
+#' Calculate Death and Survival Outcomes from COVID-19
+#'
+#' @inheritParams death_table_total
+#'
+#' @return A `tibble`
+#'
+#' @keywords internal
+death_calc_total <- function(
+  data = coviData::process_positive_people(date = date),
+  date = NULL
+) {
+  data %>%
+    dplyr::mutate(.id_tmp_ = dplyr::row_number()) %>%
+    dplyr::transmute(
+      .died_tmp_ = .data[[".id_tmp_"]] %in% filter_deaths(.)[[".id_tmp_"]],
+      died = dplyr::if_else(.data[[".died_tmp_"]], "Yes", "No")
+    ) %>%
+    janitor::tabyl(.data[["died"]]) %>%
+    janitor::adorn_totals() %>%
+    dplyr::arrange(c(2L, 1L, 3L)) %>%
+    dplyr::as_tibble()
 }
