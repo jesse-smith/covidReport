0d10a6dd17637fea01dcbce19df0e76a23fa5bf7
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-09 14:06:06 -0500

    Create, document, and export `active_table_sex()` and `active_calc_sex()`

---
+++ R/active_table_sex.R
@@ -0,0 +1,54 @@
+#' Tabluate Active Cases by Sex
+#'
+#' @param data NBS case data, as returned by
+#'   \code{\link[coviData:read-nbs]{pos(process_inv())}}
+#'
+#' @param date The download date of the data; defaults to most recent
+#'
+#' @return A `flextable`
+#'
+#' @export
+active_table_sex <- function(
+  data = pos(process_inv(read_inv(date))),
+  date = NULL
+) {
+  data %>%
+    active_calc_sex(date = date) %>%
+    active_table_(grp_lbl = "Sex") %>%
+    flextable::autofit()
+}
+
+#' Calculate Active Case Rates and Percentages by Age
+#'
+#' @inheritParams active_table_sex
+#'
+#' @return A `tibble`
+#'
+#' @export
+#'
+#' @keywords internal
+active_calc_sex <- function(
+  data = pos(process_inv(read_inv(date))),
+  date = NULL
+) {
+  data %>%
+    filter_active(date = date) %>%
+    active_trans_sex() %>%
+    active_calc_(grp = "sex")
+}
+
+active_trans_sex <- function(data) {
+  dplyr::transmute(
+    data,
+    grp = .data[["patient_current_sex"]] %>%
+      stringr::str_to_upper() %>%
+      stringr::str_remove_all("[^FMALE]") %>%
+      stringr::str_extract("^[MF]"),
+    grp = dplyr::case_when(
+      .data[["grp"]] == "F" ~ "Female",
+      .data[["grp"]] == "M" ~ "Male"
+    ) %>%
+      factor() %>%
+      forcats::fct_explicit_na("Missing")
+  )
+}
