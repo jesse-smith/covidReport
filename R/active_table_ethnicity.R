68429279206c63fa82eea0a49d1c13813960c140
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-10 09:07:24 -0500

    Create `active_table_ethnicity()`

---
+++ R/active_table_ethnicity.R
@@ -0,0 +1,49 @@
+#' Tabluate Active Cases by Race
+#'
+#' @param data NBS case data, as returned by
+#'   \code{\link[coviData:read-nbs]{pos(process_inv())}}
+#'
+#' @param date The download date of the data; defaults to most recent
+#'
+#' @return A `flextable`
+#'
+#' @export
+active_table_ethnicity <- function(
+  data = pos(process_inv(read_inv(date))),
+  date = NULL
+) {
+  data %>%
+    active_calc_ethnicity(date = date) %>%
+    active_table_(grp_lbl = "Race") %>%
+    flextable::autofit()
+}
+
+#' Calculate Active Case Rates and Percentages by Race
+#'
+#' @inheritParams active_table_sex
+#'
+#' @return A `tibble`
+#'
+#' @export
+#'
+#' @keywords internal
+active_calc_ethnicity <- function(
+  data = pos(process_inv(read_inv(date))),
+  date = NULL
+) {
+  data %>%
+    filter_active(date = date) %>%
+    active_trans_ethnicity() %>%
+    active_calc_("ethnicity")
+}
+
+active_trans_ethnicity <- function(data) {
+  dplyr::transmute(
+    data,
+    grp = dplyr::case_when(
+      .data[["patient_ethnicity"]] == "2135-2" ~ "Hispanic/Latino",
+      .data[["patient_ethnicity"]] == "2186-5" ~ "Not Hispanic/Latino",
+      TRUE ~ NA_character_
+    )
+  )
+}
